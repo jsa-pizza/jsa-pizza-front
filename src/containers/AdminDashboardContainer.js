acdc183e17088f3b2fff8b4584daf8a73aaa3807
hackertaco <imsa1224@naver.com>
2020-04-22 16:15:47 +0900

    feat: ⚡dashboard page

---
+++ src/containers/AdminDashboardContainer.js
@@ -0,0 +1,28 @@
+import React, { useCallback, useEffect,useState } from 'react';
+import AdminDashboard from '../components/AdminDashboard'
+import * as api from 'lib/api';
+
+export default function AdminDashboardContainer(){
+    const [users, setUsers] =  useState('')
+    const [comments, setComments] =  useState([])
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+          try {
+            api.dashboard().then((res) => {
+              console.log(res);
+              setUsers(res.data);
+              setComments(res.data.comments);
+            });
+          } catch (e) {}
+        };
+        fetchUsers();
+      }, []);
+    
+    return(
+     <AdminDashboard
+     users={users}
+     comments={comments}
+     ></AdminDashboard>
+    )
+}
